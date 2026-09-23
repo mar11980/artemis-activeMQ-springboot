8075f210f7eda842fe6450fca232c24d3ef12a4d
marcel <marcelewane@yahoo.fr>
2026-09-23 13:15:22 +0200

    new files

---
+++ src/test/java/com/springboot/activemq/messaging/ActiveMQApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.activemq.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveMQApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
